fd5bd4091c952db567091b036659ef405dc4b05c
Luca Iocchi <iocchi@dis.uniroma1.it>
2014-07-29 08:12:06 +0200

    new format for date and time of experiment

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -552,7 +552,7 @@ int main(int argc, char** argv){	//pass TEAMSIZE GRAPH ALGORITHM
     
     time (&rawtime);
     timeinfo = localtime(&rawtime);
-    sprintf(strnow,"%d%02d%02d%02d%02d%02d",  timeinfo->tm_year+1900,timeinfo->tm_mon+1,timeinfo->tm_mday,timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec);
+    sprintf(strnow,"%d%02d%02d_%02d%02d%02d",  timeinfo->tm_year+1900,timeinfo->tm_mon+1,timeinfo->tm_mday,timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec);
     printf("Date-time of the experiment: %s\n",strnow);
     
     // File to log all the idlenesses of an experimental scenario
